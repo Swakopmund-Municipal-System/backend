68cb990c003568fa639b4a1ebc3cd770ca71ad8b
Sakaria Nghivafe <sakarianghivafe@gmail.com>
2025-05-26 22:32:04 +0200

    fixes: fixed events urls again again

--- events/events/urls.py
+++ events/events/urls.py
@@ -1,42 +1,23 @@
-"""
-URL configuration for admin project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-"""
-URL Configuration for the Calendar/Events Microservice
-"""
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from .views import (
     EventViewSet, LocationViewSet, OrganizerViewSet, DepartmentViewSet,
     TagViewSet, AttendeeViewSet, AttachmentViewSet, RecurringPatternViewSet,
     EventChangeLogViewSet
 )
 
-router = DefaultRouter()
-router.register(r'events', EventViewSet)
-router.register(r'locations', LocationViewSet)
-router.register(r'organizers', OrganizerViewSet)
-router.register(r'departments', DepartmentViewSet)
-router.register(r'tags', TagViewSet)
-router.register(r'attendees', AttendeeViewSet)
-router.register(r'attachments', AttachmentViewSet)
-router.register(r'recurring-patterns', RecurringPatternViewSet)
-router.register(r'event-changes', EventChangeLogViewSet)
 
 # Optional API versioning prefix
 urlpatterns = [
-    path('', include(router.urls)),
+    path('events/', EventViewSet.as_view()),
+    path('locations/', LocationViewSet.as_view()),
+    path('organizers/', OrganizerViewSet.as_view()),
+    path('departments/', DepartmentViewSet.as_view()),
+    path('tags/', TagViewSet.as_view()),
+    path('attendees/', AttendeeViewSet.as_view()),
+    path('attachments/', AttachmentViewSet.as_view()),
+    path('recurring-patterns/', RecurringPatternViewSet.as_view()),
+    path('event-changes/', EventChangeLogViewSet.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
