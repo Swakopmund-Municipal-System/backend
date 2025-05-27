111866499d22418442a1fab794a9145099a0baa3
Sakaria Nghivafe <sakarianghivafe@gmail.com>
2025-05-27 16:04:43 +0200

    fixes: integrated poi admin

--- poi-service/service/urls.py
+++ poi-service/service/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('api/poi/djangoadmin/', admin.site.urls),
     path('api/poi/', include('poi.urls')) 
 ]
